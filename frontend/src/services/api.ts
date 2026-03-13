4f795478b8d07434f7cf97fb0b5aa6cb6065209d
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-03-13 22:07:56 +0530

    done change

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'http://13.235.243.35:3000/api',
   withCredentials: true,
 })
 
