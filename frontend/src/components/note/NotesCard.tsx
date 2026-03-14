77df51169538a53b8ff0340ddb74526f903f0c85
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-03-14 12:33:00 +0530

    notesGrid comoplete

--- frontend/src/components/note/NotesCard.tsx
+++ frontend/src/components/note/NotesCard.tsx
@@ -1,7 +1,30 @@
 // import React from 'react'
 
 function NotesCard() {
-  return <div>NotesCard</div>
+  return <div className="bg-white border border-slate-200 rounded-xl p-5 hover:shadow-xl transition-all cursor-pointer">
+    
+          <div className="flex justify-between items-center mb-3 ">
+                 <span className="bg-teal-100 text-teal-700 text-xs font-semibold px-2 py-1 rounded ">
+                    work
+                 </span>
+          </div>
+
+             <h3 className="font-bold text-lg mb-2">
+                 Project Brainstorming
+             </h3>
+
+             <p className="text-slate-500 text-sm line-clamp-2">
+                 Discussion about architecture
+                 -first
+                 -second
+             </p>
+
+
+             <div className="mt-4 text-xs text-slate-400">
+                 Updated 2 min ago
+             </div>
+    
+    </div>
 }
 
 export default NotesCard
