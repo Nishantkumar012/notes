44903374b1678687da8892c688fc31a94cc474f1
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-03-20 16:52:29 +0530

    make the getNotes feature

--- frontend/src/components/note/NotesCard.tsx
+++ frontend/src/components/note/NotesCard.tsx
@@ -1,6 +1,22 @@
 // import React from 'react'
+type Note = {
+  // _id: string;
+  title: string
+  content: string
+}
 
-function NotesCard() {
+// function NotesCard({ note }: { note: Note }) {
+//   return (
+//     <div className="p-4 bg-white shadow rounded-lg">
+//       <h2 className="text-lg font-semibold">{note.title}</h2>
+//       <p className="text-sm text-gray-600 mt-2">{note.content}</p>
+//     </div>
+//   );
+// }
+
+// export default NotesCard;
+
+function NotesCard({ note }: { note: Note }) {
   return (
     <div className="bg-white border border-slate-200 rounded-xl p-5 hover:shadow-xl transition-all cursor-pointer">
       <div className="flex justify-between items-center mb-3 ">
@@ -9,11 +25,9 @@ function NotesCard() {
         </span>
       </div>
 
-      <h3 className="font-bold text-lg mb-2">Project Brainstorming</h3>
+      <h3 className="font-bold text-lg mb-2">{note.title}</h3>
 
-      <p className="text-slate-500 text-sm line-clamp-2">
-        Discussion about architecture -first -second
-      </p>
+      <p className="text-slate-500 text-sm line-clamp-2">{note.content}</p>
 
       <div className="mt-4 text-xs text-slate-400">Updated 2 min ago</div>
     </div>
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -6,4 +6,29 @@ const api = axios.create({
   withCredentials: true,
 })
 
+// 🔥 REQUEST INTERCEPTOR
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  // console.log(token)
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+
+  return config
+})
+
+// 🔥 Response interceptor (optional)
+api.interceptors.response.use(
+  (res) => res,
+  (err) => {
+    if (err.response?.status === 401) {
+      console.log('Unauthorized → redirect to login')
+      // optional:
+      // window.location.href = "/login";
+    }
+    return Promise.reject(err)
+  }
+)
+
 export default api
