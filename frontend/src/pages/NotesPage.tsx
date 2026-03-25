4995ec3cd2207866b4ced4714ff444e3404101f3
Nishantkumar012 <124617537+Nishantkumar012@users.noreply.github.com>
2026-03-25 19:05:30 +0530

    litl

--- frontend/src/pages/NotesPage.tsx
+++ frontend/src/pages/NotesPage.tsx
@@ -1,12 +1,12 @@
 // import React from 'react'
-import NoteForm from '../components/NoteForm'
-import NoteCard from '../components/NoteCard'
+// import NoteForm from '../components/NoteForm'
+// import NoteCard from '../components/NoteCard'
 
 export default function NotesPage() {
   return (
     <div>
-      <NoteForm />
-      <NoteCard />
+
+          <p>No need</p>
     </div>
   )
 }
